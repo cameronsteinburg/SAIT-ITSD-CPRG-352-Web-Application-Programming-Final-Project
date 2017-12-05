2dfd32b1d9eb14786f0f60a4f886f1017ec2b9f1
734972 <734972@ELF6IT215940.ACDM.DS.SAIT.CA>
2017-12-04 19:49:22 -0700

    company admins added with all their proper functionality and permissions. My notes still dont update around in the admin users window, not sure why

--- src/java/servlets/CompanyAdminServlet.java
+++ src/java/servlets/CompanyAdminServlet.java
@@ -1,11 +1,21 @@
 package servlets;
 
+import businesslogic.UserService;
+import dataaccess.CompanyDB;
+import dataaccess.UserDB;
+import domainmodel.Company;
+import domainmodel.User;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -15,11 +25,178 @@ public class CompanyAdminServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        UserService us = new UserService();
+
+        String action = request.getParameter("action");
+
+        if (action != null && action.equals("view")) {
+
+            String selectedUsername = request.getParameter("selectedUsername");
+
+            try {
+
+                User user = us.get(selectedUsername);
+                request.setAttribute("selectedUser", user);
+
+            } catch (Exception ex) {
+                Logger.getLogger(CompanyAdminServlet.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            request.setAttribute("message", "Edit User Below");
+        }
+
+        List<Company> comps = null;
+        CompanyDB cdb = new CompanyDB();
+        comps = cdb.getAll();
+
+        request.setAttribute("comps", comps);
+
+        List<User> users = null;
+
+        try {
+            users = us.getAll();
+        } catch (Exception ex) {
+            Logger.getLogger(CompanyAdminServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        ArrayList<User> mathcingUsers = new ArrayList<User>();
+        HttpSession session = request.getSession();
+
+        User curuser = (User) session.getAttribute("curuser");
+
+        String thisCompany = curuser.getCompany().getCompanyName();
+        request.setAttribute("thisCompany", thisCompany);
+
+        int curuserCompanyID = curuser.getCompany().getCompanyID();
+
+        for (int i = 0; i < users.size(); i++) {
+            if (users.get(i).getCompany().getCompanyID() == curuserCompanyID) {
+                mathcingUsers.add(users.get(i));
+            }
+        }
+
+        request.setAttribute("users", mathcingUsers);
+
         getServletContext().getRequestDispatcher("/WEB-INF/admin/companyUsers.jsp").forward(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+       
+        HttpSession session = ((HttpServletRequest) request).getSession();
+        User curuser = (User) session.getAttribute("curuser");
         
+        request.setAttribute("thisCompany", curuser.getCompany().getCompanyName());
+        
+        String action = request.getParameter("action");
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        String email = request.getParameter("email");
+        String firstname = request.getParameter("firstname");
+        String lastname = request.getParameter("lastname");
+        boolean active = request.getParameter("active") != null;
+
+        UserService us = new UserService();
+        
+        
+
+        try {
+            
+            if (action.equals("delete")) {
+                
+
+                String selectedUsername = request.getParameter("selectedUsername");
+
+                UserDB userdb = new UserDB();
+
+                String user = (String) session.getAttribute("username");
+
+                if (selectedUsername.equals(user)) {
+                    //you cant delete yourself!
+                    request.setAttribute("message", "Whoops.  You can't delete yourself.");
+
+                    action = request.getParameter("action");
+
+                    if (action != null && action.equals("view")) {
+                        selectedUsername = request.getParameter("selectedUsername");
+
+                        try {
+
+                            User newuser = us.get(selectedUsername);
+                            request.setAttribute("selectedUser", user);
+
+                            int id = newuser.getCompany().getCompanyID();
+                            request.setAttribute("oldCompanyID", id);
+
+                        } catch (Exception ex) {
+                            Logger.getLogger(CompanyAdminServlet.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    }
+
+                    List<User> users = null;
+                    try {
+                        users = us.getAll();
+
+                    } catch (Exception ex) {
+                        Logger.getLogger(CompanyAdminServlet.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+
+                    ArrayList<User> mathcingUsers = new ArrayList<User>();
+                    curuser = (User) session.getAttribute("curuser");
+                    int curuserCompanyID = curuser.getCompany().getCompanyID();
+
+                    for (int i = 0; i < users.size(); i++) {
+                        if (users.get(i).getCompany().getCompanyID() == curuserCompanyID) {
+                            mathcingUsers.add(users.get(i));
+                        }
+                    }
+
+                    request.setAttribute("users", mathcingUsers);
+
+                    getServletContext().getRequestDispatcher("/WEB-INF/admin/companyUsers.jsp").forward(request, response);
+                    return;
+                }
+
+                us.delete(selectedUsername);
+                request.setAttribute("message", "Account Successfully Deleted!");
+
+            } else if (action.equals("edit")) {
+
+                Company thisCompany = curuser.getCompany();
+                us.update(username, password, email, active, firstname, lastname, thisCompany);
+                request.setAttribute("message", "Account Successfully Updated!");
+
+            } else if (action.equals("add")) {
+
+                Company thisCompany = curuser.getCompany();
+                us.insert(username, password, email, active, firstname, lastname, thisCompany);
+                request.setAttribute("message", "Account Successfully Added!");
+            }
+
+        } catch (Exception ex) {
+            request.setAttribute("message", "Whoops.  Could not perform that action.");
+        }
+
+        List<User> users = null;
+
+        try {
+            users = us.getAll();
+        } catch (Exception ex) {
+            Logger.getLogger(CompanyAdminServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        ArrayList<User> mathcingUsers = new ArrayList<User>();
+        
+        int curuserCompanyID = curuser.getCompany().getCompanyID();
+
+        for (int i = 0; i < users.size(); i++) {
+            if (users.get(i).getCompany().getCompanyID() == curuserCompanyID) {
+                mathcingUsers.add(users.get(i));
+            }
+        }
+
+        request.setAttribute("users", mathcingUsers);
+
+        getServletContext().getRequestDispatcher("/WEB-INF/admin/companyUsers.jsp").forward(request, response);
     }
 }
--- web/WEB-INF/admin/companyUsers.jsp
+++ web/WEB-INF/admin/companyUsers.jsp
@@ -1,7 +1,86 @@
 <%@include file="/WEB-INF/jspf/header.jspf" %>
-        <title>Company Users</title>
-    </head>
-    <body>
-        <h1>Manage the Users In Your Company</h1>
-    </body>
+
+<title>Company Users</title>
+</head>
+<body>
+    <h1>Manage the Users in Your Company</h1>
+    <br>
+    <br>
+    ${message}
+    <br>
+    <br>
+    <table border="1">
+        <tr>
+            <th>Username</th>
+            <th>Firstname</th>
+            <th>Lastname</th>
+            <th>E-mail</th>
+            <th>Role</th>
+            <th>Note</th>
+            <th>Company</th>
+            <th>Delete</th>
+            <th>Edit</th>
+        </tr>
+        <c:forEach var="user" items="${users}">
+            <tr>
+                <td>${user.username}</td>
+                <td>${user.firstname}</td>
+                <td>${user.lastname}</td>
+                <td>${user.email}</td>
+                <td>${user.role.roleName}</td>
+                <td>
+                    <ul>
+                        <c:forEach var="note" items="${user.noteCollection}">
+                            <li>${note.title}</li>
+                            </c:forEach>
+                    </ul>
+                </td>
+                <td>${user.company.companyName}</td>
+                <td>
+                    <form action="companyAdmin" method="post" >
+                        <input type="submit" value="Delete">
+                        <input type="hidden" name="action" value="delete">
+                        <input type="hidden" name="selectedUsername" value="${user.username}">
+                    </form>
+                </td>
+                <td>
+                    <form action="companyAdmin" method="get">
+                        <input type="submit" value="Edit">
+                        <input type="hidden" name="action" value="view">
+                        <input type="hidden" name="selectedUsername" value="${user.username}">
+                    </form>
+                </td>
+            </tr>
+        </c:forEach>
+    </table>
+    <c:if test="${selectedUser == null}">
+        <h3>Add User</h3>
+        <form action="companyAdmin" method="POST">
+            username: <input type="text" name="username"><br>
+            first name: <input type="text" name="firstname"><br>
+            last name: <input type="text" name="lastname"><br>
+            password: <input type="password" name="password"><br>
+            email: <input type="email" name="email"><br>
+            company: ${thisCompany}<br>
+            active?: <input type="checkbox" name="active"><br>
+            <input type="hidden" name="action" value="add">
+            <input type="submit" value="Save">
+        </form>
+    </c:if>
+    <c:if test="${selectedUser != null}">
+        <h3>Edit User</h3>
+        <form action="companyAdmin" method="POST">
+            username: <input type="text" name="username" value="${selectedUser.username}" readonly><br>
+            first name: <input type="text" name="firstname" value="${selectedUser.firstname}"><br>
+            last name: <input type="text" name="lastname" value="${selectedUser.lastname}"><br>
+            password: <input type="password" name="password" value="${selectedUser.password}"><br>
+            email: <input type="email" name="email" value="${selectedUser.email}"><br>
+            company: ${thisCompany}<br>
+            active?: <input type="checkbox" name="active" ${selectedUser.active ? "checked" : ""}><br>
+            <input type="hidden" name="action" value="edit">
+            <input type="submit" value="Save">
+        </form>
+    </c:if>
+
+</body>
 </html>
